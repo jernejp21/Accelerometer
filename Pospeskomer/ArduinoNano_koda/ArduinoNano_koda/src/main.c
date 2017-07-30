b01b4b4ccc45494695b49eb39821c6028d7c9840
jernej <jernej@5772e67a-4b97-4f9b-a58a-b09a45319703>
2017-07-30 11:28:04 +0000

    Dodana začetna koda za projekt pospeškomer - blink
    git-svn-id: file:///D/zacasno/Nova%20mapa/SVN@1 5772e67a-4b97-4f9b-a58a-b09a45319703

---
+++ Pospeskomer/ArduinoNano_koda/ArduinoNano_koda/src/main.c
@@ -0,0 +1,52 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+
+#define F_CPU 16000000L
+#define BAUD 9600
+
+#include <asf.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <util/setbaud.h>
+#include "Serial.h"
+
+int main (void)
+{
+	// Initialization code
+	DDRB |= (1<<PB5);
+
+	while(1)
+	{
+		PORTB ^= (1<<PB5);
+		_delay_ms(100);
+		
+	}
+
+	// Insert application code here, after the board has been initialized.
+}
